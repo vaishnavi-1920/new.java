668caaa444f75b5e99fe6e997e9c1a324d8474a8
vaishnavi <abc@gmail.com>
2025-03-04 19:11:57 +0530

    added java files

---
+++ demo.java
@@ -0,0 +1,7 @@
+// This is a simple "Hello World" program in Java
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
