db6da76a4bf11d6a6e32596f5bea08e7a4d499a8
vaishnavi-1920 <shettyvaishnavi474@gmail.com>
2025-03-04 19:28:18 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
 // This is a simple "Hello World" program in Java
-public class HelloWorld {
+public class demo.java {
     public static void main(String[] args) {
         // Prints "Hello, World!" to the console
         System.out.println("Hello, World!");
